b20ca56083ff158b40088bb31264d71ab9e6b79c
axela <axel.antunes@utbm.fr>
2023-10-09 17:13:18 +0200

    init

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000; // Change this to the desired port
+
+// Middleware for parsing JSON requests
+app.use(bodyParser.json());
+
+// Temporary storage for stories (in-memory array)
+const stories = [];
+
+// Endpoint to submit a new story
+app.post('/api/submit-story', (req, res) => {
+    const { author, content } = req.body;
+
+    // Basic validation: Check if author and content are provided
+    if (!author || !content) {
+        return res.status(400).json({ error: 'Author and content are required.' });
+    }
+
+    // Create a new story object
+    const newStory = { author, content };
+
+    // Add the new story to the array
+    stories.push(newStory);
+
+    res.status(201).json(newStory);
+});
+
+// Endpoint to retrieve all stories
+app.get('/api/get-stories', (req, res) => {
+    res.json(stories);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ src/writing.ts
@@ -0,0 +1,86 @@
+// Define an interface for a story
+interface Story {
+    author: string;
+    content: string;
+}
+
+// Function to submit a new story
+async function submitStory() {
+    // Get the author and story content from the form
+    const authorInput = document.getElementById("author") as HTMLInputElement;
+    const storyInput = document.getElementById("story") as HTMLInputElement;
+    const author = authorInput.value;
+    const content = storyInput.value;
+
+    // Create a new story object
+    const newStory: Story = { author, content };
+
+    try {
+        // Send a POST request to the server to submit the story
+        const response = await fetch("/api/submit-story", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newStory),
+        });
+
+        if (response.ok) {
+            // Story submission was successful
+            const jsonResponse = await response.json();
+            // Add the new story to the page
+            displayStory(jsonResponse);
+            // Clear the form inputs
+            authorInput.value = "";
+            storyInput.value = "";
+        } else {
+            // Handle submission error (e.g., display an error message)
+            console.error("Error submitting story");
+        }
+    } catch (error) {
+        console.error("Network error:", error);
+    }
+}
+
+// Function to display a story on the page
+function displayStory(story: Story) {
+    const storiesContainer = document.getElementById("stories-container");
+
+    // Create a new story element
+    const storyElement = document.createElement("div");
+    storyElement.classList.add("story");
+    storyElement.innerHTML = `
+        <h3>${story.author}</h3>
+        <p>${story.content}</p>
+    `;
+
+    // Add the story to the container
+    storiesContainer.appendChild(storyElement);
+}
+
+// Event listener for the form submission
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    submitStory();
+});
+
+// Initial page load: Retrieve and display existing stories (if any)
+async function loadStories() {
+    try {
+        const response = await fetch("/api/get-stories");
+        if (response.ok) {
+            const stories = await response.json();
+            for (const story of stories) {
+                displayStory(story);
+            }
+        } else {
+            console.error("Error loading stories");
+        }
+    } catch (error) {
+        console.error("Network error:", error);
+    }
+}
+
+// Call the loadStories function to load stories when the page loads
+loadStories();
