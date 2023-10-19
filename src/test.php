8b9baed457dea691c7689a1ba96d96272e5f6a7f
axela <axel.antunes@utbm.fr>
2023-10-20 01:10:57 +0200

    V1.4 UI IMPROVED, COMMENTS WORKS

--- src/test.php
+++ src/test.php
@@ -9,6 +9,6 @@ $link = new mysqli($host_name, $user_name, $password, $database);
 if ($link->connect_error) {
     die('<p>La connexion au serveur MySQL a échoué: '. $link->connect_error .'</p>');
 } else {
-    echo '<p>Connexion au serveur MySQL établie avec succès.</p>';
+    echo '<p>Connexio au serveur MySQL établie avec succès.</p>';
 }
 ?>
