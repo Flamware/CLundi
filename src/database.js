a77c5c6bc5113e0cdd1b7e348e1efbd7fdcbb585
axela <axel.antunes@utbm.fr>
2023-10-24 02:59:19 +0200

    V1.6.1 DATABASE UPDATED

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 const { Client } = require("pg");
 const fs = require("fs");
 const readline = require("readline");
-
+const con="postgresql://axel:BHlG7j676OPb7u6uFJhf_Q@punchy-dragon-11093.8nj.cockroachlabs.cloud:26257/clundi?sslmode=verify-full"
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: con,
     ssl: {
         rejectUnauthorized: false,
     }
@@ -18,48 +18,5 @@ async function connectDatabase() {
         console.error("Error connecting to the database:", error);
     }
 }
-
-// ... (previous code)
-
-async function importData() {
-    const csvFilePath = "../stories.csv"; // Update with the actual file path
-
-    console.log("Importing data from CSV to the database");
-
-    const fileStream = fs.createReadStream(csvFilePath);
-
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-    });
-
-    // Skip the first line containing column headers
-    let firstLineSkipped = false;
-
-    for await (const line of rl) {
-        if (!firstLineSkipped) {
-            // Skip the first line
-            firstLineSkipped = true;
-            continue;
-        }
-
-        // Split the line into two columns using '\t' as the delimiter (excluding story_id)
-        const [author, content] = line.split('\t');
-
-        // Check if author and content are not null or empty before insertion
-        if (author && content) {
-            try {
-                const query = "INSERT INTO public.stories (author, content) VALUES ($1, $2)";
-                await client.query(query, [author, content]);
-            } catch (error) {
-                console.error("Error inserting data:", error);
-            }
-        }
-    }
-
-    console.log("Data imported from CSV to the database");
-}
-module.exports = {
-    client,
-    connectDatabase,
-};
+exports.connectDatabase = connectDatabase;
+exports.client = client;
