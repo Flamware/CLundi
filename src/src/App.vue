628c6c9d99ccf2f5eee7ab6bdd2fe19ee52712b2
axela <axel.antunes@utbm.fr>
2024-02-02 00:58:56 +0100

    V2.0
    -Fixing Dockerfile

---
+++ src/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="background-container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+.background-container {
+  background-image: url('./assets/1014.jpg'); /* Adjust the path based on your project structure */
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+
+}
+
+
+
+</style>
+
+<script setup>
+</script>
