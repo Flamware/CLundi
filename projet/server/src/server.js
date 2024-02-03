d99b87efc1e53b545b39076f78c70f9cae9212e8
Flamware <axel.antunes@utbm.fr>
2024-02-03 04:50:05 +0100

    V2 - Route to server for ALL

--- projet/server/src/server.js
+++ projet/server/src/server.js
@@ -329,12 +329,8 @@ app.delete('/delete-comment/:commentId', verifyToken, (req, res) => {
     });
 });
 
-
 const httpServer = http.createServer(app);
-
-
-
-httpServer.listen(portHTTP, () => {
+httpServer.listen(portHTTP, '0.0.0.0', () => {
     console.log(`HTTP Server is running on port ${portHTTP}`);
-    console.log(`Server IP: ${ip.address()}`); // Print the local IP address
+    console.log('Server is accessible externally');
 });
