782c960fb2440942457786b633658c86b4e74a8b
Flamware <axel.antunes@utbm.fr>
2024-02-03 04:55:47 +0100

    V2 - Route to server for ALL

--- projet/client/src/axios-conf.js
+++ projet/client/src/axios-conf.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set the base URL globally for Axios
-axios.defaults.baseURL = 'http://87.106.121.11:3000/';
+axios.defaults.baseURL = 'https://87.106.121.11:3000/';
 
 axios.interceptors.request.use((config) => {
     // You can modify headers or perform other actions before the request is sent
