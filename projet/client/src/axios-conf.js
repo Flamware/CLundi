45e286ab38d7c6774478d303938daf8a3d9b21b4
Flamware <axel.antunes@utbm.fr>
2024-02-03 12:07:19 +0100

    V2 - SSL for server

--- projet/client/src/axios-conf.js
+++ projet/client/src/axios-conf.js
@@ -1,10 +1,12 @@
+// axios-conf.js
 import axios from 'axios';
+import https from 'https';
 
 // Set the base URL globally for Axios
 axios.defaults.baseURL = 'https://87.106.121.11:8445';
 
+// Ignore SSL verification (for development purposes only)
 axios.interceptors.request.use((config) => {
-    config.httpsAgent = new https.Agent({ rejectUnauthorized: false });
     return config;
 });
 
