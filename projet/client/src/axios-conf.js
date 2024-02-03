390d0220c6a697ac681f7f96001d83cab238dfe3
Flamware <axel.antunes@utbm.fr>
2024-02-03 04:10:23 +0100

    V2 - Route to server for ALL

--- projet/client/src/axios-conf.js
+++ projet/client/src/axios-conf.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set the base URL globally for Axios
-axios.defaults.baseURL = 'https://clundi.fr/3000'; // Replace with your API base URL
+axios.defaults.baseURL = 'https://clundi.fr:3000/'; // Replace with your API base URL
 
 axios.interceptors.request.use((config) => {
     // You can modify headers or perform other actions before the request is sent
